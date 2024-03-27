1496cfb72aefce4abfed2231092749ed1a85682b
Tomasz Piwoda <tomasz.piwoda@gmail.com>
2024-03-27 12:05:15 +0100

    Added Status enum that represents our data base status and how it can change. Created Epic struct that represents epics. Story struct represents stories in our data base. Finally DataBaseState struct with last_item and mapping for epics and stories.

---
+++ src/models.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed
+}
+
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub Status: Status,
+    pub stories: Vec<u32>
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        todo!()
+    }
+}
+
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        todo!()
+    }
+}
+
+pub struct DataBaseState {
+    pub last_item: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>
+}
