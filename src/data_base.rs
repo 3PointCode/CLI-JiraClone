59a5f1e57868359a0b968bc20bf57fce1237eefa
Tomasz Piwoda <tomasz.piwoda@gmail.com>
2024-03-27 12:57:18 +0100

    data_base module included in main.rs. File data_base.rs added which includes JSONFileDataBase with file_path field. Trait Database created that has 2 functions read_db and write_db. The triat is implemented by JSONFileDatabase struct and I made an implementation block for Database trait with test module that tests if implemented functions are working.

---
+++ src/data_base.rs
@@ -0,0 +1,96 @@
+use std::fs;
+
+use anyhow::{Ok, Result};
+
+use crate::models::{DataBaseState, Epic, Story, Status};
+
+trait Database {
+    fn read_db(&self) -> Result<DataBaseState>;
+    fn write_db(&self, db_state: &DataBaseState) -> Result<()>;
+}
+
+struct JSONFileDatabase {
+    pub file_path: String
+}
+
+impl Database for JSONFileDatabase {
+    fn read_db(&self) -> Result<DataBaseState> {
+        let database_content = fs::read_to_string(&self.file_path)?;
+        let parsed: DataBaseState = serde_json::from_str(&database_content)?;
+        Ok(parsed)
+    }
+
+    fn write_db(&self, database_state: &DataBaseState) -> Result<()> {
+        fs::write(&self.file_path, &serde_json::to_vec(database_state)?)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod database {
+        use std::collections::HashMap;
+        use std::io::Write;
+
+        use super::*;
+
+        #[test]
+        fn read_db_should_fail_with_invalid_path() {
+            let database = JSONFileDatabase { file_path: "INVALID_PATH".to_owned() };
+            assert_eq!(database.read_db().is_err(), true);
+        }
+
+        #[test]
+        fn read_db_should_fail_with_invalid_json() {
+            let mut tmp_file = tempfile::NamedTempFile::new().unwrap();
+            let file_contents = r#"{ "last_item_id": 0 epics: {} stories {} }"#;
+            write!(tmp_file, "{}", file_contents).unwrap();
+
+            let database = JSONFileDatabase { file_path: tmp_file.path().to_str()
+                        .expect("Failed to convert tmp_file path to str").to_string() };
+            let result = database.read_db();
+
+            assert_eq!(result.is_err(), true);
+        }
+
+        #[test]
+        fn read_db_should_parse_json_file() {
+            let mut tmp_file = tempfile::NamedTempFile::new().unwrap();
+            let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
+            write!(tmp_file, "{}", file_contents).unwrap();
+
+            let database = JSONFileDatabase { file_path: tmp_file.path().to_str()
+                        .expect("Failed to convert tmp_file to str").to_string() };
+            let result = database.read_db();
+
+            assert_eq!(result.is_ok(), true);
+        }
+
+        #[test]
+        fn write_to_db_should_work() {
+            let mut tmp_file = tempfile::NamedTempFile::new().unwrap();
+            let file_contents = r#"{ "last_item_id": 0, "epics": {}, "stories": {} }"#;
+            write!(tmp_file, "{}", file_contents).unwrap();
+
+            let database = JSONFileDatabase { file_path: tmp_file.path().to_str()
+                        .expect("Failed to convert tmp_file path to str").to_string() };
+            let story = Story { name: "epic 1".to_owned(), description: "epic 1".to_owned(), status: Status::Open };
+            let epic = Epic { name: "epic 1".to_owned(), description: "epic 1".to_owned(), status: Status::Open, stories: vec![2] };
+            
+            let mut stories = HashMap::new();
+            stories.insert(2, story);
+            let mut epics = HashMap::new();
+            epics.insert(1, epic);
+
+            let state = DataBaseState { last_item_id: 2, epics, stories };
+
+            let write_result = database.write_db(&state);
+            let read_result = database.read_db().unwrap();
+
+            assert_eq!(write_result.is_ok(), true);
+            assert_eq!(read_result, state);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod models;
+mod data_base;
 
 fn main() {
     println!("Welcome to my Jira clone!");
