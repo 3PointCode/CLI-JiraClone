2cc3c62641c8ef46c5c5f3bddf16e0bd81965062
Tomasz Piwoda <tomasz.piwoda@gmail.com>
2024-06-24 21:11:09 +0200

    Implemented trait Page for HomePage struct.

--- src/user_interface/pages/mod.rs
+++ src/user_interface/pages/mod.rs
@@ -1,5 +1,6 @@
 use std::any::Any;
 use std::rc::Rc;
+use anyhow::Ok;
 use anyhow::Result;
 use anyhow::anyhow;
 use crate::data_base::JiraDatabase;
@@ -17,4 +18,48 @@ pub trait Page {
 
 pub struct HomePage {
     pub db: Rc<JiraDatabase>
+}
+
+impl Page for HomePage {
+    fn draw_page(&self) -> Result<()> {
+        println!("----------------------------- EPICS -----------------------------");
+        println!("     id     |               name               |      status      ");
+
+        let epics = self.db.read_db()?.epics;
+
+        for id in epics.keys().sorted() {
+            let epic = &epics[id];
+            let id_col = get_column_string(&id.to_string(), 11);
+            let name_col = get_column_string(&epic.name, 32);
+            let status_col = get_column_string(&epic.status.to_string(), 17);
+            println!("{} | {} | {}", id_col, name_col, status_col);
+        }
+
+        println!();
+        println!();
+
+        println!("[q] quit | [c] create epic | [:id:] navigate to epic");
+        Ok(())
+    }
+
+    fn handle_input(&self, input: &str) -> Result<Option<Action>> {
+        let epics = self.db.read_db()?.epics;
+
+        match input {
+            "q" => Ok(Some(Action::Exit)),
+            "c" => Ok(Some(Action::CreateEpic)),
+            input => {
+                if let Ok(epic_id) = input.parse::<u32>() {
+                    if epics.contains_key(&epic_id) {
+                        return Ok(Some(Action::NavigateToEpicDetail { epic_id }));
+                    }
+                }
+                Ok(None)
+            }
+        }
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
