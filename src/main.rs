f950bd617910725a3c13afc5c811d419e02c2b22
Tomasz Piwoda <tomasz.piwoda@gmail.com>
2024-07-02 12:36:28 +0200

    Added neccessery imports into main.rs file.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 mod models;
+
 mod data_base;
+use data_base::*;
+
 mod user_interface;
+
+mod io_utils;
+use io_utils::*;
+
+use std::rc::Rc;
 
 fn main() {
     println!("Welcome to my Jira clone!");
