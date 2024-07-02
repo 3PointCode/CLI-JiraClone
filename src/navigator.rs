2567509778e5f6ccd7ada9f2911b6585461da46b
Tomasz Piwoda <tomasz.piwoda@gmail.com>
2024-07-02 14:38:16 +0200

    Added handle_action() function that is responsible for making right actions in our CLI with users input.

--- src/navigator.rs
+++ src/navigator.rs
@@ -1,5 +1,6 @@
-use crate::{user_interface::{Page, HomePage, EpicDetail, StoryDetail, Prompts}, data_base::JiraDatabase, models::Action};
+use crate::{data_base::JiraDatabase, models::{Action, Status}, user_interface::{EpicDetail, HomePage, Page, Prompts, StoryDetail}};
 use std::rc::Rc;
+use anyhow::{anyhow, Result, Context, Ok};
 
 pub struct Navigator {
     pages: Vec<Box<dyn Page>>,
@@ -14,6 +15,63 @@ impl Navigator {
 
     pub fn get_current_page(&self) -> Option<&Box<dyn Page>> {
         self.pages.last()
+    }
+
+    pub fn handle_action(&mut self, action: Action) -> Result<()> {
+        match action {
+            Action::NavigateToEpicDetail { epic_id } => {
+                self.pages.push(Box::new(EpicDetail { epic_id, db: Rc::clone(&self.db) }));
+            }
+            Action::NavigateToStoryDetail { epic_id, story_id } => {
+                self.pages.push(Box::new(StoryDetail { epic_id, story_id, db: Rc::clone(&self.db) }));
+            }
+            Action::NavigateToPreviousPage => {
+                if !self.pages.is_empty() { self.pages.pop(); }
+            }
+            Action::CreateEpic => {
+                let epic = (self.prompts.create_epic)();
+                self.db.create_epic(epic).with_context(|| anyhow!("Failed to create Epic!"))?;
+            }
+            Action::UpdateEpicStatus { epic_id } => {
+                let status = (self.prompts.update_status)();
+                if let Some(status) = status{
+                    self.db.update_epic_status(epic_id, status).with_context(|| anyhow!("Failed to update Epic status!"))?;
+                }
+            }
+            Action::DeleteEpic { epic_id } => {
+                if (self.prompts.delete_epic)() {
+                    self.db.delete_epic(epic_id).with_context(|| anyhow!("Failed to delete Epic!"))?;
+
+                    if !self.pages.is_empty() {
+                        self.pages.pop();
+                    }
+                }
+            }
+            Action::CreateStory { epic_id } => {
+                let story = (self.prompts.create_story)();
+                self.db.create_story(story, epic_id).with_context(|| anyhow!("Failed to create Story!"))?;
+            }
+            Action::UpdateStoryStatus { story_id } => {
+                let status = (self.prompts.update_status)();
+                if let Some(status) = status {
+                    self.db.update_story_status(story_id, status).with_context(|| anyhow!("Failed to update Story status!"))?;
+                }
+            }
+            Action::DeleteStory { epic_id, story_id } => {
+                if (self.prompts.delete_story)() {
+                    self.db.delete_story(epic_id, story_id).with_context(|| anyhow!("Failed to delete Story!"))?;
+
+                    if !self.pages.is_empty() {
+                        self.pages.pop();
+                    }
+                }
+            }
+            Action::Exit => {
+                self.pages.clear()
+            },
+        }
+        
+        Ok(())
     }
 
     fn get_page_count(&self) -> usize {
